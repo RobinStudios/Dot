25b6ed0d7537e3a8b56b8c42548739933bd94969
RobinStudios <danteburner9@gmail.com>
2025-08-10 00:18:39 -0700

    Fix next.config.js invalid options

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   env: {
     NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-1',
     NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID,
-  },
-  serverRuntimeConfig: {
-    REPLICATE_API_TOKEN: process.env.REPLICATE_API_TOKEN,
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
   images: {
     domains: ['localhost'],
